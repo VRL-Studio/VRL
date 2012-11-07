deddf3bf6f3fc04a35cb7e9c86b6334cda2c581f
Michael Hoffer <info@michaelhoffer.de>
2012-11-07 16:48:23 +0100

    we are compatible with groovy 1.8 again

--- VRL/src/eu/mihosoft/vrl/asm/EmptyVisitor.java
+++ VRL/src/eu/mihosoft/vrl/asm/EmptyVisitor.java
@@ -4,74 +4,74 @@
  */
 package eu.mihosoft.vrl.asm;
 
-import groovyjarjarasm.asm.AnnotationVisitor;
-import groovyjarjarasm.asm.ClassVisitor;
-import groovyjarjarasm.asm.FieldVisitor;
-import groovyjarjarasm.asm.MethodVisitor;
-import groovyjarjarasm.asm.Opcodes;
+//import groovyjarjarasm.asm.AnnotationVisitor;
+//import groovyjarjarasm.asm.ClassVisitor;
+//import groovyjarjarasm.asm.FieldVisitor;
+//import groovyjarjarasm.asm.MethodVisitor;
+//import groovyjarjarasm.asm.Opcodes;
 
-/**
- *
- * @author Michael Hoffer <info@michaelhoffer.de>
- */
-public class EmptyVisitor extends ClassVisitor {
-
-        AnnotationVisitor av = new AnnotationVisitor(Opcodes.ASM4) {
-
-            @Override
-            public AnnotationVisitor visitAnnotation(String name, String desc) {
-                return this;
-            }
-
-            @Override
-            public AnnotationVisitor visitArray(String name) {
-                return this;
-            }
-        };
-
-        public EmptyVisitor() {
-            super(Opcodes.ASM4);
-        }
-
-        @Override
-        public AnnotationVisitor visitAnnotation(String desc, boolean visible) {
-            return av;
-        }
-
-        @Override
-        public FieldVisitor visitField(int access, String name, String desc,
-                String signature, Object value) {
-            return new FieldVisitor(Opcodes.ASM4) {
-
-                @Override
-                public AnnotationVisitor visitAnnotation(String desc,
-                        boolean visible) {
-                    return av;
-                }
-            };
-        }
-
-        @Override
-        public MethodVisitor visitMethod(int access, String name, String desc,
-                String signature, String[] exceptions) {
-            return new MethodVisitor(Opcodes.ASM4) {
-
-                @Override
-                public AnnotationVisitor visitAnnotationDefault() {
-                    return av;
-                }
-
-                @Override
-                public AnnotationVisitor visitAnnotation(String desc,
-                        boolean visible) {
-                    return av;
-                }
-
-                @Override
-                public AnnotationVisitor visitParameterAnnotation(
-                        int parameter, String desc, boolean visible) {
-                    return av;
-                }
-            };
-        }
-    }
+///**
+// *
+// * @author Michael Hoffer <info@michaelhoffer.de>
+// */
+//public class EmptyVisitor extends ClassVisitor {
+//
+//        AnnotationVisitor av = new AnnotationVisitor(Opcodes.ASM4) {
+//
+//            @Override
+//            public AnnotationVisitor visitAnnotation(String name, String desc) {
+//                return this;
+//            }
+//
+//            @Override
+//            public AnnotationVisitor visitArray(String name) {
+//                return this;
+//            }
+//        };
+//
+//        public EmptyVisitor() {
+//            super(Opcodes.ASM4);
+//        }
+//
+//        @Override
+//        public AnnotationVisitor visitAnnotation(String desc, boolean visible) {
+//            return av;
+//        }
+//
+//        @Override
+//        public FieldVisitor visitField(int access, String name, String desc,
+//                String signature, Object value) {
+//            return new FieldVisitor(Opcodes.ASM4) {
+//
+//                @Override
+//                public AnnotationVisitor visitAnnotation(String desc,
+//                        boolean visible) {
+//                    return av;
+//                }
+//            };
+//        }
+//
+//        @Override
+//        public MethodVisitor visitMethod(int access, String name, String desc,
+//                String signature, String[] exceptions) {
+//            return new MethodVisitor(Opcodes.ASM4) {
+//
+//                @Override
+//                public AnnotationVisitor visitAnnotationDefault() {
+//                    return av;
+//                }
+//
+//                @Override
+//                public AnnotationVisitor visitAnnotation(String desc,
+//                        boolean visible) {
+//                    return av;
+//                }
+//
+//                @Override
+//                public AnnotationVisitor visitParameterAnnotation(
+//                        int parameter, String desc, boolean visible) {
+//                    return av;
+//                }
+//            };
+//        }
+//    }
