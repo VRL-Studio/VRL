fc2dd3c323c0f20390d8de56e9c5d68eff4f657f
Michael Hoffer <info@michaelhoffer.de>
2013-01-28 17:20:20 +0100

    - fixed a bug that showed wron linenumbers for non-project classes - repository & entry updated (equals compares name and version)

--- VRL/src/eu/mihosoft/vrl/system/Repository.java
+++ VRL/src/eu/mihosoft/vrl/system/Repository.java
@@ -4,6 +4,7 @@
  */
 package eu.mihosoft.vrl.system;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,17 +12,20 @@ import java.util.List;
  *
  * @author Michael Hoffer <info@michaelhoffer.de>
  */
-public class Repository {
+public class Repository implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
     private List<RepositoryEntry> entries =
             new ArrayList<RepositoryEntry>();
 
     public Repository() {
     }
-    
+
     public void addEntry(RepositoryEntry entry) {
         entries.add(entry);
     }
-    
+
     public boolean removeEntry(RepositoryEntry entry) {
         return entries.remove(entry);
     }
@@ -39,6 +43,4 @@ public class Repository {
     public void setEntries(List<RepositoryEntry> entries) {
         this.entries = entries;
     }
-    
-    
 }
--- VRL/src/eu/mihosoft/vrl/system/RepositoryEntry.java
+++ VRL/src/eu/mihosoft/vrl/system/RepositoryEntry.java
@@ -4,12 +4,16 @@
  */
 package eu.mihosoft.vrl.system;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Michael Hoffer <info@michaelhoffer.de>
  */
-public class RepositoryEntry {
+public class RepositoryEntry implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+    
     private String name;
     private String version;
     private String SHA1Checksum;
@@ -81,4 +85,41 @@ public class RepositoryEntry {
     public void setUrl(String url) {
         this.url = url;
     }
+
+    @Override
+    public String toString() {
+        return "name:     " + getName() + "\n"
+                + "version:  " + getVersion() + "\n"
+                + "checksum: " + getSHA1Checksum() + "\n"
+                + "url:      " + getUrl() + "\n";
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final RepositoryEntry other = (RepositoryEntry) obj;
+        if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
+            return false;
+        }
+        if ((this.version == null) ? (other.version != null) : !this.version.equals(other.version)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + (this.name != null ? this.name.hashCode() : 0);
+        hash = 23 * hash + (this.version != null ? this.version.hashCode() : 0);
+        return hash;
+    }
+    
+    
 }
