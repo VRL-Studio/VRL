d47b2b270c8d36b0ff5b8490dc2cf4d031da554b
Michael Hoffer <info@michaelhoffer.de>
2013-01-30 13:38:44 +0100

    security classes documented

--- VRL/src/eu/mihosoft/vrl/security/PGPUtil.java
+++ VRL/src/eu/mihosoft/vrl/security/PGPUtil.java
@@ -6,27 +6,63 @@ package eu.mihosoft.vrl.security;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.security.Security;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openpgp.PGPException;
 
 /**
+ * Utility class for PGP key generation, signing and verification. This class is
+ * only functional if the bcpg-*.jar files are on the classpath. As these
+ * Jar-Files are signed they cannot be included in the VRL.jar.
+ * <p>
+ * These methods use the PGP implementation from http://www.bouncycastle.org/ .
+ * </p>
  *
  * @author Michael Hoffer <info@michaelhoffer.de>
  */
 public class PGPUtil {
+    
+    {
+        Security.addProvider(new BouncyCastleProvider());
+    }
 
     private PGPUtil() {
         throw new AssertionError();
     }
 
+    /**
+     * Creates an RSA key pair for PGP signing and verification.
+     *
+     * @param identity the identity, e.g., "mycompany.com"
+     * @param password the password for the private key
+     * @param pubKeyFile public key file (will be created/overwritten)
+     * @param privKeyFile private key file (will be created/overwritten)
+     * @param ascii defines whether to use ASCII format
+     * @throws IOException if key files cannot be writen
+     * @throws PGPException if PGP algorithms failed or security provider is not
+     * accepted
+     */
     public static void createKeyPair(
             String identity, String password,
-            File pubKeyFile, File privKeyFile, boolean ascii) throws IOException {
+            File pubKeyFile, File privKeyFile, boolean ascii) throws IOException, PGPException {
         RSAKeyPairGenerator.createKeyPair(
                 identity, password, ascii, pubKeyFile, privKeyFile);
     }
 
+    /**
+     * Signs a file. This method creates a signature file for the specified
+     * file.
+     *
+     * @param privKeyFile the private key
+     * @param password the password for the private key
+     * @param file the file to sign
+     * @param signatureFile the signature file (will be created/overwritten)
+     * @param ascii defines whether to use ASCII format
+     * @throws IOException if files cannot be found or the signature cannot be
+     * written
+     * @throws PGPException if PGP algorithms failed or security provider is not
+     * accepted
+     */
     public static void signFile(File privKeyFile, String password,
             File file, File signatureFile, boolean ascii) throws IOException, PGPException {
         DetachedSignatureProcessor.signFile(privKeyFile,
@@ -34,8 +70,21 @@ public class PGPUtil {
                 signatureFile, ascii);
     }
 
+    /**
+     * Verifies a file. This method uses a signature file and a public key to
+     * verify the specified file.
+     *
+     * @param pubKeyFile public key file
+     * @param file file to verify
+     * @param signatureFile signature file
+     * @return <code>true</code> if verification was successful;
+     * <code>false</code> otherwise
+     * @throws IOException if files cannot be read
+     * @throws PGPExceptionif PGP algorithms failed or security provider is not
+     * accepted
+     */
     public static boolean verifyFile(
-            File pubKeyFile, String password, File file, File signatureFile)
+            File pubKeyFile, File file, File signatureFile)
             throws IOException, PGPException {
 
         return DetachedSignatureProcessor.verifyFile(pubKeyFile, file, signatureFile);
