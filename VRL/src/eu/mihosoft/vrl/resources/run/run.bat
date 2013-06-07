2f0fb0be8be7f68a56b4dffc1f13a7636beb60a6
Michael Hoffer <info@michaelhoffer.de>
2013-06-07 15:31:47 +0200

    improved run.bat for cosole task (untested!)

--- VRL/src/eu/mihosoft/vrl/resources/run/run.bat
+++ VRL/src/eu/mihosoft/vrl/resources/run/run.bat
@@ -1,10 +1,12 @@
 @echo off
-set CONF=-enable3d yes -resolution 1024x768 -defaultProject default.vrlp -property-folder-suffix default -plugin-checksum-test no
 
 set APPDIR="%CD%\.application"
-
 cd "%APPDIR%"
 
+set PROPERTY_FOLDER="%APPDIR%"\property-folder
+set PROJECT_FILE="project.jar"
+
+set CONF=-property-folder %PROPERTY_FOLDER% -plugin-checksum-test no -install-plugin-help no -install-payload-plugins yes
 
 set LIBDIR32=%pwd%lib\windows\x86;custom-lib\windows\x86
 set LIBDIR64=%pwd%lib\windows\x64;custom-lib\windows\x64
@@ -22,10 +24,13 @@ if defined ProgramW6432 (
   set MAXHEAP=512
 )
 
+REM if no integrated jre can be found try using the system version
+if not exist %JAVAEXE% set JAVAEXE=java
+
 REM version of 2011
 REM start /min /realtime %JAVAEXE% -Xms64m -Xmx512m -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled -XX:MaxPermSize=256m -splash:resources\studio-resources\splashscreen.png -Djava.library.path="%LIBDIR%" -jar VRL-Studio.jar %CONF%
 
 REM optimized for jre 7 (19.04.2012)
-start /min /realtime %JAVAEXE% -Xms64m -Xmx%MAXHEAP%m -XX:MaxPermSize=256m -splash:resources\studio-resources\splashscreen.png -Djava.library.path="%LIBDIR%" -jar VRL-Studio.jar %CONF% %*
+start /max /realtime %JAVAEXE% -Xms64m -Xmx%MAXHEAP%m -XX:MaxPermSize=256m -Djava.library.path="%LIBDIR%" -jar "%PROJECT_FILE%" %CONF%
 
 exit
