1a17ffaa07e3ed64b0cdad7f4fae7774526434fd
Michael Hoffer <info@michaelhoffer.de>
2014-01-09 23:41:43 +0100

    new tests added

--- VRL/test/eu/mihosoft/vrl/io/VIOTest.java
+++ VRL/test/eu/mihosoft/vrl/io/VIOTest.java
@@ -25,21 +25,15 @@ import org.junit.Test;
  */
 public class VIOTest {
 
-    File testDir;
-    File projectDir;
-    File testPropertyFolder;
-    VProjectController projectController;
+    private static File testDir;
+    private static File projectDir;
+    private static File testPropertyFolder;
+    private static File projectFile;
+    private static VProject project;
+    private static VProjectController projectController;
 
     @BeforeClass
     public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() {
         testDir = new File(new File("build"), "test-tmp");
         testDir.mkdirs();
 
@@ -50,12 +44,23 @@ public class VIOTest {
         testPropertyFolder.mkdirs();
 
         VRL.initAll(new String[]{"-property-folder", testPropertyFolder.getAbsolutePath()});
-        
+
         JPanel canvasParent = new JPanel();
         VisualCanvas canvas = new VisualCanvas();
         canvasParent.add(canvas);
 
         projectController = new VProjectController(canvasParent, null);
+
+        projectFile = new File(projectDir, "project-01.vrlp");
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+
     }
 
     @After
@@ -65,20 +70,94 @@ public class VIOTest {
     @Test
     public void createProjectTest() {
 
+        Assert.assertTrue("saving project must not throw exception!", createProject());
+    }
+
+    @Test
+    public void openProjectTest() {
+
+        Assert.assertTrue("loading project must not throw exception!", openProject());
+
+        Assert.assertTrue("closing project must not throw exception!", closeProject());
+
+    }
+
+    private boolean createProject() {
         VProjectSessionCreator saver
                 = new VProjectSessionCreator(null);
-
-        File project = new File(projectDir, "project-01.vrlp");
-
-        boolean throwsExcepion = true;
-
+        boolean success = false;
         try {
-            saver.saveFile(projectController, project, ".vrlp");
-            throwsExcepion = false;
+            saver.saveFile(projectController, projectFile, ".vrlp");
+            success = true;
         } catch (IOException ex) {
             Logger.getLogger(VIOTest.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return success;
+    }
 
-        Assert.assertTrue("saving project must not throw exception!", throwsExcepion == false);
+    private boolean openProject() {
+        boolean success = false;
+        try {
+            projectController.loadProject(projectFile, false);
+            success = true;
+        } catch (IOException ex) {
+            Logger.getLogger(VIOTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return success;
+    }
+    
+    private boolean saveProject() {
+        boolean success = false;
+        try {
+            projectController.saveProject(false);
+            success = true;
+        } catch (IOException ex) {
+            Logger.getLogger(VIOTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return success;
+    }
+
+    private boolean closeProject() {
+        boolean success = false;
+        try {
+            projectController.closeProject();
+            success = true;
+        } catch (IOException ex) {
+            Logger.getLogger(VIOTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return success;
+    }
+
+    @Test
+    public void addComponentToCanvas() {
+        Assert.assertTrue("loading project must not throw exception!", openProject());
+
+        boolean success = false;
+
+        try {
+            projectController.getCurrentCanvas().addObject(new String("This is a test"));
+            success = true;
+        } catch (Exception ex) {
+            Logger.getLogger(VIOTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        Assert.assertTrue("adding object must not throw exception!", success);
+        
+        Assert.assertTrue("saving project must not throw exception!", saveProject());
+
+        Assert.assertTrue("closing project must not throw exception!", closeProject());
+
+        Assert.assertTrue("loading project must not throw exception!", openProject());
+
+        int numObjects = projectController.getCurrentCanvas().getInspector().getObjects().size();
+        
+        Assert.assertTrue("project must contain exactly one objects!: contains " + numObjects,
+                numObjects == 1);
+        
+        int numWindows = projectController.getCurrentCanvas().getWindows().size();
+        
+        Assert.assertTrue("canvas must contain exactly 3 windows (start, stop, string)! contains " + numWindows,
+                numWindows == 3);
+
     }
 }
