2ebe914a405b21271698342d166b9f31147d1c2f
Michael Hoffer <info@michaelhoffer.de>
2014-02-06 18:16:49 +0100

    code reader reset test implemented

--- VRL/VRL-Lang/src/test/java/eu/mihosoft/vrl/lang/model/CodeRangeTest.java
+++ VRL/VRL-Lang/src/test/java/eu/mihosoft/vrl/lang/model/CodeRangeTest.java
@@ -23,24 +23,24 @@ import org.junit.Test;
  * @author Michael Hoffer &lt;info@michaelhoffer.de&gt;
  */
 public class CodeRangeTest {
-    
+
     @BeforeClass
     public static void setUpClass() {
         //
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
-    
+
     @Test
     public void testValidReadRangeTest() {
 
@@ -70,10 +70,10 @@ public class CodeRangeTest {
         // 4:\n
         // 5:class B] {}\n
         parameterizedValidReadRangeTestLineAndColumn(code, 2, 1, 5, 7, "/ comment 1\n// comment 2\n\nclass B");
-        
+
         parameterizedValidReadRangeTestLineAndColumn(code, 0, 0, 0, 0, "");
     }
-    
+
     @Test
     public void rangeIntersectionTest() {
         // note:
@@ -104,16 +104,16 @@ public class CodeRangeTest {
         // 4:\n
         // 5:class B {}]
         Reader codeReader = new StringReader(code);
-        
+
         ICodeRange a = new CodeRange(6, 44, codeReader);
         ICodeRange b = new CodeRange(2, 5, 5, 10, codeReader);
-        
+
         String expectedCode = "mment 1\n// comment 2\n\nclass";
-        
+
         ICodeRange intersection = a.intersection(b);
-        
+
         ICodeReader intersectionReader = new CodeReader(codeReader);
-        
+
         boolean readSuccess = false;
         String intersectionCode = "";
         try {
@@ -122,52 +122,92 @@ public class CodeRangeTest {
         } catch (IOException ex) {
             Logger.getLogger(CodeRangeTest.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         Assert.assertTrue("Reader must not throw an exception!", readSuccess);
-        
+
         Assert.assertTrue("Intersection [" + expectedCode.replace("\n", "\\n") + "] expected, got [" + intersectionCode.replace("\n", "\\n") + "]", expectedCode.equals(intersectionCode));
     }
-    
+
+    @Test
+    public void resetReaderTest() {
+
+        // note:
+        // - newline (\n, \r\n, \r) is treatet as single char!
+        // - [ and ] are used to define text ranges
+        //
+        String code = String.join("\n",
+                "class A {}",
+                "",
+                "// comment 1",
+                "// comment 2",
+                "",
+                "class B {}", "");
+
+        Reader reader = new StringReader(code);
+
+        ICodeRange range = new CodeRange(1, 4, reader);
+
+        ICodeReader codeReader = new CodeReader(reader);
+
+        boolean readSuccess = false;
+
+        String firstResult = "";
+        String secondResult = "";
+
+        try {
+            firstResult = codeReader.read(range);
+            secondResult = codeReader.read(range);
+            readSuccess = true;
+        } catch (IOException ex) {
+            Logger.getLogger(CodeRangeTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        Assert.assertTrue("Reader must not throw an exception", readSuccess);
+        Assert.assertTrue("First result must not be empty", !firstResult.isEmpty());
+        Assert.assertTrue("Second result must not be empty", !secondResult.isEmpty());
+        Assert.assertTrue("First result must be equal to second result: [" + firstResult + "] == [" + secondResult + "]", firstResult.equals(secondResult));
+    }
+
     private void parameterizedValidReadRangeTestCharIndex(String code, int beginCharIdx, int endCharIdx, String expectedSubString) {
         Reader codeReader = new StringReader(code);
         ICodeRange range = new CodeRange(beginCharIdx, endCharIdx, codeReader);
-        
+
         ICodeReader reader = new CodeReader(codeReader);
-        
+
         boolean successRead = false;
-        
+
         String rangeCode = "";
-        
+
         try {
             rangeCode = reader.read(range);
             successRead = true;
         } catch (IOException ex) {
             Logger.getLogger(CodeRangeTest.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         Assert.assertTrue("Reader must not throw an exception", successRead);
         Assert.assertEquals("[" + expectedSubString + "] expected, got [" + rangeCode + "]", expectedSubString, rangeCode);
     }
-    
+
     private void parameterizedValidReadRangeTestLineAndColumn(String code, int beginL, int beginC, int endL, int endC, String expectedSubString) {
         Reader codeReader = new StringReader(code);
         ICodeRange range = new CodeRange(beginL, beginC, endL, endC, codeReader);
-        
+
         ICodeReader reader = new CodeReader(codeReader);
-        
+
         boolean successRead = false;
-        
+
         String rangeCode = "";
-        
+
         try {
             rangeCode = reader.read(range);
             successRead = true;
         } catch (IOException ex) {
             Logger.getLogger(CodeRangeTest.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         Assert.assertTrue("Reader must not throw an exception", successRead);
         Assert.assertTrue("[" + rangeCode.replace("\n", "\\n") + "] expected, got [" + expectedSubString.replace("\n", "\\n") + "]", expectedSubString.equals(rangeCode));
     }
-    
+
 }
