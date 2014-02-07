7f6a48d73b2caf0194b164c968a890608067643c
Michael Hoffer <info@michaelhoffer.de>
2014-02-07 21:46:41 +0100

    user can decide whther close the input stream in comment parser after parsing

--- VRL/VRL-Lang/src/main/java/eu/mihosoft/vrl/lang/VCommentParser.java
+++ VRL/VRL-Lang/src/main/java/eu/mihosoft/vrl/lang/VCommentParser.java
@@ -17,6 +17,8 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.antlr.v4.runtime.ANTLRInputStream;
+import static org.antlr.v4.runtime.ANTLRInputStream.INITIAL_BUFFER_SIZE;
+import static org.antlr.v4.runtime.ANTLRInputStream.READ_BUFFER_SIZE;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
@@ -32,8 +34,14 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker;
  */
 public class VCommentParser {
 
-    public static void parse(InputStream is, CommentsListener l) throws IOException {
-        ANTLRInputStream input = new ANTLRInputStream(is);
+    public static void parse(InputStream is, CommentsListener l, boolean closeStream) throws IOException {
+        final ANTLRInputStream input;
+        
+        if (closeStream) {
+             input = new ANTLRInputStream(is);
+        } else {
+            input = new ANTLRInputStreamNoClose(is);
+        }
 
         CommentsLexer lexer = new CommentsLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -45,9 +53,15 @@ public class VCommentParser {
         CommentsListener extractor = l;
         walker.walk(extractor, tree);
     }
-    
-    public static void parse(Reader is, CommentsListener l) throws IOException {
-        ANTLRInputStream input = new ANTLRInputStream(is);
+
+    public static void parse(Reader is, CommentsListener l, boolean closeStream) throws IOException {
+        final ANTLRInputStream input;
+        
+        if (closeStream) {
+             input = new ANTLRInputStream(is);
+        } else {
+            input = new ANTLRInputStreamNoClose(is);
+        }
 
         CommentsLexer lexer = new CommentsLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -64,44 +78,8 @@ public class VCommentParser {
         final List<Comment> result = new ArrayList<>();
 
         final Reader reader = is;
-        final ANTLRInputStream input = new ANTLRInputStream(is) {
-            @Override
-            public void load(Reader r, int size, int readChunkSize)
-                    throws IOException {
-                if (r == null) {
-                    return;
-                }
-                if (size <= 0) {
-                    size = INITIAL_BUFFER_SIZE;
-                }
-                if (readChunkSize <= 0) {
-                    readChunkSize = READ_BUFFER_SIZE;
-                }
-                // System.out.println("load "+size+" in chunks of "+readChunkSize);
-                try {
-                    // alloc initial buffer size.
-                    data = new char[size];
-                    // read all the data in chunks of readChunkSize
-                    int numRead = 0;
-                    int p = 0;
-                    do {
-                        if (p + readChunkSize > data.length) { // overflow?
-                            // System.out.println("### overflow p="+p+", data.length="+data.length);
-                            data = Arrays.copyOf(data, data.length * 2);
-                        }
-                        numRead = r.read(data, p, readChunkSize);
-                        // System.out.println("read "+numRead+" chars; p was "+p+" is now "+(p+numRead));
-                        p += numRead;
-                    } while (numRead != -1); // while not EOF
-                    // set the actual size of the data available;
-                    // EOF subtracted one above in p+=numRead; add one back
-                    n = p + 1;
-                    //System.out.println("n="+n);
-                } finally {
-                    //r.close();
-                }
-            }
-        };
+
+        final ANTLRInputStream input = new ANTLRInputStreamNoClose(is);
 
         final CommentsLexer lexer = new CommentsLexer(input);
         final CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -184,4 +162,81 @@ public class VCommentParser {
 
         return result;
     }
+}
+
+class ANTLRInputStreamNoClose extends ANTLRInputStream {
+
+    public ANTLRInputStreamNoClose() {
+        super();
+    }
+
+    public ANTLRInputStreamNoClose(String input) {
+        super(input);
+    }
+
+    public ANTLRInputStreamNoClose(char[] data, int numberOfActualCharsInArray) {
+        super(data, numberOfActualCharsInArray);
+    }
+
+    public ANTLRInputStreamNoClose(Reader r) throws IOException {
+        super(r);
+    }
+
+    public ANTLRInputStreamNoClose(Reader r, int initialSize) throws IOException {
+        super(r, initialSize);
+    }
+
+    public ANTLRInputStreamNoClose(Reader r, int initialSize, int readChunkSize) throws IOException {
+        super(r, initialSize, readChunkSize);
+    }
+
+    public ANTLRInputStreamNoClose(InputStream input) throws IOException {
+        super(input);
+    }
+
+    public ANTLRInputStreamNoClose(InputStream input, int initialSize) throws IOException {
+        super(input, initialSize);
+    }
+
+    public ANTLRInputStreamNoClose(InputStream input, int initialSize, int readChunkSize) throws IOException {
+        super(input, initialSize, readChunkSize);
+    }
+
+    @Override
+    public void load(Reader r, int size, int readChunkSize)
+            throws IOException {
+        if (r == null) {
+            return;
+        }
+        if (size <= 0) {
+            size = INITIAL_BUFFER_SIZE;
+        }
+        if (readChunkSize <= 0) {
+            readChunkSize = READ_BUFFER_SIZE;
+        }
+        // System.out.println("load "+size+" in chunks of "+readChunkSize);
+        try {
+            // alloc initial buffer size.
+            data = new char[size];
+            // read all the data in chunks of readChunkSize
+            int numRead = 0;
+            int p = 0;
+            do {
+                if (p + readChunkSize > data.length) { // overflow?
+                    // System.out.println("### overflow p="+p+", data.length="+data.length);
+                    data = Arrays.copyOf(data, data.length * 2);
+                }
+                numRead = r.read(data, p, readChunkSize);
+                // System.out.println("read "+numRead+" chars; p was "+p+" is now "+(p+numRead));
+                p += numRead;
+            } while (numRead != -1); // while not EOF
+            // set the actual size of the data available;
+            // EOF subtracted one above in p+=numRead; add one back
+            n = p + 1;
+            //System.out.println("n="+n);
+        } finally {
+            r.close();
+        }
+    }
+
 }
