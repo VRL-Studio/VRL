ffb6fc62302f06fe7ae15ba30279496360204203
Michael Hoffer <info@michaelhoffer.de>
2014-07-23 17:55:23 +0200

    loop sample added

---
+++ VRL/VRL-UI/ForSample.groovy
@@ -0,0 +1,115 @@
+public class MyFileClass {
+    
+    public void m(int v1) {
+        for(int i = 0; i <= 10; i++) {
+            Integer x;
+            x=2+v1;
+        }
+    }
+}
+// <editor-fold defaultstate="collapsed" desc="VRL-Data">
+/*<!VRL!><Type:VRL-Layout>
+<map>
+  <entry>
+    <string>Script:MyFileClass:m:for:var=i:inv:op ASSIGN</string>
+    <layout>
+      <x>791.8445781769066</x>
+      <y>151.9653335738232</y>
+      <width>200.0</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script</string>
+    <layout>
+      <x>0.0</x>
+      <y>0.0</y>
+      <width>1140.5265200517463</width>
+      <height>800.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MyFileClass:m:for:var=i:inv:declare i</string>
+    <layout>
+      <x>0.0</x>
+      <y>0.0</y>
+      <width>200.0</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MyFileClass:m:for:var=i:inv:declare x</string>
+    <layout>
+      <x>377.72423440582264</x>
+      <y>24.354867958346002</y>
+      <width>200.0</width>
+      <height>157.61626302882615</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MyFileClass:m:for:var=i:inv:op PLUS</string>
+    <layout>
+      <x>375.68321864646623</x>
+      <y>234.40432579734627</y>
+      <width>200.0</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MyFileClass</string>
+    <layout>
+      <x>0.0</x>
+      <y>0.0</y>
+      <width>336.66614196928606</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MyFileClass:m</string>
+    <layout>
+      <x>316.1619038471332</x>
+      <y>105.23869811090047</y>
+      <width>381.06753281154874</width>
+      <height>172.57120799602177</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MyFileClass:inv:declare this</string>
+    <layout>
+      <x>0.0</x>
+      <y>0.0</y>
+      <width>200.0</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MyFileClass:m:for:var=i:inv:scope</string>
+    <layout>
+      <x>0.0</x>
+      <y>0.0</y>
+      <width>527.5049749051605</width>
+      <height>206.78490698100825</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MyFileClass:m:for:var=i</string>
+    <layout>
+      <x>0.0</x>
+      <y>0.0</y>
+      <width>527.5049749051605</width>
+      <height>206.78490698100825</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+</map>
+*/
+// </editor-fold>
