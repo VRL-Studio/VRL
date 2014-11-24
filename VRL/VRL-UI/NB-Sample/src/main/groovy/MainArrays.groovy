60d71889dc543f6a75aa9f1a94913bffe132f7d2
Michael Hoffer <info@michaelhoffer.de>
2014-11-24 14:48:22 +0100

    partial fix for issue #40 : https://github.com/VRL-Studio/VRL/issues/40
    - array-access-support added (e.g. args[3] )

---
+++ VRL/VRL-UI/NB-Sample/src/main/groovy/MainArrays.groovy
@@ -0,0 +1,165 @@
+public class MainArrays {
+
+    
+    public static void main(String[] args) {
+        String s;
+        s = (args[0]);
+        println(s);
+    }
+}
+// <editor-fold defaultstate="collapsed" desc="VRL-Data">
+/*<!VRL!><Type:VRL-Layout>
+<map>
+  <entry>
+    <string>Script</string>
+    <layout>
+      <x>0.0</x>
+      <y>0.0</y>
+      <width>576.2533569335938</width>
+      <height>443.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MainArrays:main:inv:op ACCESS_ARRAY_ELEMENT</string>
+    <layout>
+      <x>248.8755317662245</x>
+      <y>211.20303081799412</y>
+      <width>365.50875226229124</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MainArrays:main:inv:op ASSIGN:0</string>
+    <layout>
+      <x>747.9309233618131</x>
+      <y>247.91748683798082</y>
+      <width>324.98484005369664</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MainArrays:main:inv:declare args</string>
+    <layout>
+      <x>0.0</x>
+      <y>0.0</y>
+      <width>229.88190908398693</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MainArrays:main:inv:println:0</string>
+    <layout>
+      <x>1194.414669888256</x>
+      <y>134.36854328075532</y>
+      <width>215.55118711319096</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MainArrays:main:0</string>
+    <layout>
+      <x>0.0</x>
+      <y>163.8108096467495</y>
+      <width>339.0699764061807</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MainArrays:main:inv:declare s:0</string>
+    <layout>
+      <x>325.41814543218226</x>
+      <y>1.6131666212084046</y>
+      <width>200.0</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MainArrays:inv:declare this</string>
+    <layout>
+      <x>0.963592997922056</x>
+      <y>0.0</y>
+      <width>200.0</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MainArrays:main:inv:declare args:0</string>
+    <layout>
+      <x>0.0</x>
+      <y>0.0</y>
+      <width>229.88190908398693</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MainArrays:main:inv:println</string>
+    <layout>
+      <x>1194.414669888256</x>
+      <y>134.36854328075532</y>
+      <width>215.55118711319096</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MainArrays:main:inv:op ACCESS_ARRAY_ELEMENT:0</string>
+    <layout>
+      <x>248.8755317662245</x>
+      <y>211.20303081799412</y>
+      <width>365.50875226229124</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MainArrays:main:inv:declare s</string>
+    <layout>
+      <x>325.41814543218226</x>
+      <y>1.6131666212084046</y>
+      <width>200.0</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MainArrays:main:inv:op ASSIGN</string>
+    <layout>
+      <x>747.9309233618131</x>
+      <y>247.91748683798082</y>
+      <width>324.98484005369664</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MainArrays:main</string>
+    <layout>
+      <x>0.0</x>
+      <y>163.8108096467495</y>
+      <width>339.0699764061807</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:MainArrays</string>
+    <layout>
+      <x>0.0</x>
+      <y>0.0</y>
+      <width>695.0372601585583</width>
+      <height>549.533163855855</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+</map>
+*/
+// </editor-fold>
