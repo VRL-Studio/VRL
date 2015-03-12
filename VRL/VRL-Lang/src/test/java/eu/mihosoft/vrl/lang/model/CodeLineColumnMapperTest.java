f5d863c45998958377450d7582b26ae022d0c929
turpid-monkey <s1496737@stud.uni-frankfurt.de>
2015-03-13 00:07:22 +0100

    Added some more tests

--- VRL/VRL-Lang/src/test/java/eu/mihosoft/vrl/lang/model/CodeLineColumnMapperTest.java
+++ VRL/VRL-Lang/src/test/java/eu/mihosoft/vrl/lang/model/CodeLineColumnMapperTest.java
@@ -22,7 +22,7 @@ public class CodeLineColumnMapperTest {
 		assertEquals(7, loc.getLocation(1, 2).getOffset());
 		assertEquals(16, loc.getLocation(2, 2).getOffset());
 		
-		// get locations
+		// get line, col by offset
 		assertEquals(0, loc.getLocation(2).getLine());
 		assertEquals(2, loc.getLocation(2).getColumn());
 		
@@ -36,6 +36,22 @@ public class CodeLineColumnMapperTest {
 		assertEquals(0, loc.getLocation(4).getLine());
 		assertEquals(1, loc.getLocation(5).getLine());
 		assertEquals(2, loc.getLocation(6).getLine());
+		
+		loc.init("test\n\ntest");
+		assertEquals(2, loc.getLocation(10).getLine());
+		assertEquals(4, loc.getLocation(10).getColumn());
+		assertEquals(3, loc.getLocation(11).getLine());
+		assertEquals(0, loc.getLocation(11).getColumn());
+		assertEquals(-1, loc.getLocation(12).getLine());
+		assertEquals(-1, loc.getLocation(12).getColumn());
+		
+		loc.init("test\n\ntest\n"); // TODO is this an issue??
+		assertEquals(2, loc.getLocation(10).getLine());
+		assertEquals(4, loc.getLocation(10).getColumn());
+		assertEquals(3, loc.getLocation(11).getLine());
+		assertEquals(0, loc.getLocation(11).getColumn());
+		assertEquals(-1, loc.getLocation(12).getLine());
+		assertEquals(-1, loc.getLocation(12).getColumn());
 		
 	}
 
