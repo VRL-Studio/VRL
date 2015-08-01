28d03a55d40ac0a7e1138316cb44877f86ba9ef0
Michael Hoffer <info@michaelhoffer.de>
2015-08-01 18:00:30 +0200

    basic method chaining support

--- VRL/VRL-UI/MainWithChainedInvocations.groovy
+++ VRL/VRL-UI/MainWithChainedInvocations.groovy
@@ -3,7 +3,7 @@ public class MainWithChainedInvocations {
 
     
     public my.testpackage.MainWithChainedInvocations m1() {
-        return this; 
+        return this;
     }
     public static void main() {
         my.testpackage.MainWithChainedInvocations m;
@@ -13,6 +13,16 @@ public class MainWithChainedInvocations {
 // <editor-fold defaultstate="collapsed" desc="VRL-Data">
 /*<!VRL!><Type:VRL-Layout>
 <map>
+  <entry>
+    <string>Script:my.testpackage.MainWithChainedInvocations:m1:inv:return</string>
+    <layout>
+      <x>324.3620700617288</x>
+      <y>57.24036530501096</y>
+      <width>200.0</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
   <entry>
     <string>Script:my.testpackage.MainWithChainedInvocations:main:inv:declare m</string>
     <layout>
@@ -34,12 +44,42 @@ public class MainWithChainedInvocations {
     </layout>
   </entry>
   <entry>
+    <string>Script:my.testpackage.MainWithChainedInvocations:m1</string>
+    <layout>
+      <x>45.0</x>
+      <y>181.0</y>
+      <width>309.3505859375</width>
+      <height>180.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:my.testpackage.MainWithChainedInvocations:main:inv:m1:0</string>
+    <layout>
+      <x>418.17387133220683</x>
+      <y>360.09416698051143</y>
+      <width>200.0</width>
+      <height>150.0</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
     <string>Script:my.testpackage.MainWithChainedInvocations:main</string>
     <layout>
       <x>474.5799554865798</x>
       <y>48.649098636429905</y>
       <width>323.3515625</width>
       <height>204.25137329101562</height>
+      <contentVisible>true</contentVisible>
+    </layout>
+  </entry>
+  <entry>
+    <string>Script:my.testpackage.MainWithChainedInvocations:main:inv:m1</string>
+    <layout>
+      <x>414.0950138440407</x>
+      <y>169.08879731964996</y>
+      <width>200.0</width>
+      <height>150.0</height>
       <contentVisible>true</contentVisible>
     </layout>
   </entry>
@@ -58,36 +98,6 @@ public class MainWithChainedInvocations {
     <layout>
       <x>0.0</x>
       <y>0.0</y>
-      <width>200.0</width>
-      <height>150.0</height>
-      <contentVisible>true</contentVisible>
-    </layout>
-  </entry>
-  <entry>
-    <string>Script:my.testpackage.MainWithChainedInvocations:m1:inv:return</string>
-    <layout>
-      <x>324.3620700617288</x>
-      <y>57.24036530501096</y>
-      <width>200.0</width>
-      <height>150.0</height>
-      <contentVisible>true</contentVisible>
-    </layout>
-  </entry>
-  <entry>
-    <string>Script:my.testpackage.MainWithChainedInvocations:m1</string>
-    <layout>
-      <x>45.0</x>
-      <y>181.0</y>
-      <width>309.3505859375</width>
-      <height>180.0</height>
-      <contentVisible>true</contentVisible>
-    </layout>
-  </entry>
-  <entry>
-    <string>Script:my.testpackage.MainWithChainedInvocations:main:inv:m1</string>
-    <layout>
-      <x>414.0950138440407</x>
-      <y>169.08879731964996</y>
       <width>200.0</width>
       <height>150.0</height>
       <contentVisible>true</contentVisible>
