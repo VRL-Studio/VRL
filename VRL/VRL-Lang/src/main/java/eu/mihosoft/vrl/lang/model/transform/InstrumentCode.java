10a94477eba83c6f52b6c9b53a7b10336b891525
Michael Hoffer <info@michaelhoffer.de>
2015-08-07 12:58:06 +0200

    issue #58 : invNeedsTmpVar(Invocation inv) returned false positives

--- VRL/VRL-Lang/src/main/java/eu/mihosoft/vrl/lang/model/transform/InstrumentCode.java
+++ VRL/VRL-Lang/src/main/java/eu/mihosoft/vrl/lang/model/transform/InstrumentCode.java
@@ -17,6 +17,7 @@ import eu.mihosoft.vrl.lang.model.IfDeclaration;
 import eu.mihosoft.vrl.lang.model.Invocation;
 import eu.mihosoft.vrl.lang.model.MethodDeclaration;
 import eu.mihosoft.vrl.lang.model.Operator;
+import eu.mihosoft.vrl.lang.model.ReturnStatementInvocation;
 import eu.mihosoft.vrl.lang.model.Scope;
 import eu.mihosoft.vrl.lang.model.ScopeInvocation;
 import eu.mihosoft.vrl.lang.model.Type;
@@ -164,9 +165,13 @@ class InstrumentControlFlowScope implements CodeTransform<ControlFlowScope> {
         if (inv instanceof DeclarationInvocation) {
             return false;
         }
+        
+        if (inv instanceof ReturnStatementInvocation) {
+            return false;
+        }
 
         if (!(inv instanceof BinaryOperatorInvocationImpl)) {
-            return false;
+            return true;
         }
 
         BinaryOperatorInvocationImpl boi = (BinaryOperatorInvocationImpl) inv;
