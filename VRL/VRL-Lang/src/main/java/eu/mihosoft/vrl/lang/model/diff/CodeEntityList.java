f0b66dd16de5d59cc3992d09393fe841c2c33189
JoaPiep <joanna@flash.gcsc.uni-frankfurt.de>
2016-06-01 16:29:01 +0200

    Update code entity list with clone()

--- VRL/VRL-Lang/src/main/java/eu/mihosoft/vrl/lang/model/diff/CodeEntityList.java
+++ VRL/VRL-Lang/src/main/java/eu/mihosoft/vrl/lang/model/diff/CodeEntityList.java
@@ -24,10 +24,12 @@ import org.codehaus.groovy.control.SourceUnit;
  *
  * @author Joanna Pieper <joanna.pieper@gcsc.uni-frankfurt.de>
  */
-public class CodeEntityList { // anpassen an CELIST
+public class CodeEntityList {
 
     private List<CodeEntity> entities = new ArrayList<>();
     private int index = 0;
+
+    private CompilationUnitDeclaration cud;
 
     /**
      * empty constructor
@@ -37,6 +39,7 @@ public class CodeEntityList { // anpassen an CELIST
 
     public CodeEntityList(CompilationUnitDeclaration root) {
         this.entities = convertTreeToList(root);
+        this.cud = root;
     }
 
     /**
@@ -68,20 +71,25 @@ public class CodeEntityList { // anpassen an CELIST
     public CodeEntityList(CodeEntityList entities, Boolean bool) {
 
         if (bool && !entities.getEntities().isEmpty()) {
-            
-            CompilationUnitDeclaration cud = (CompilationUnitDeclaration) getRoot(entities.get(0));         
+
+            CompilationUnitDeclaration cuDecl;
+            if (entities.get(0) instanceof CompilationUnitDeclaration) {
+                cuDecl = (CompilationUnitDeclaration) entities.get(0);
+            } else {
+                cuDecl = (CompilationUnitDeclaration) getRoot(entities.get(0));
+            }
             CompilationUnitDeclaration cudClone;
             try {
-                cudClone = clone(cud);
+                cudClone = clone(cuDecl);
                 this.entities = convertTreeToList(cudClone);
             } catch (Exception ex) {
                 Logger.getLogger(CodeEntityList.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
         } else {
             this.entities = entities.getEntities();
         }
         this.index = entities.index;
+        this.cud = entities.cud;
     }
 
     /**
@@ -95,6 +103,22 @@ public class CodeEntityList { // anpassen an CELIST
             this.index = index;
         }
 
+    }
+
+    /**
+     *
+     * @return root of the tree - CompilationUnitDeclaration
+     */
+    public CompilationUnitDeclaration getCud() {
+        return cud;
+    }
+
+    /**
+     *
+     * @param cud set root of the tree
+     */
+    public void setCud(CompilationUnitDeclaration cud) {
+        this.cud = cud;
     }
 
     /**
@@ -156,7 +180,6 @@ public class CodeEntityList { // anpassen an CELIST
         entities.set(i, entity);
     }
 
-
     /**
      *
      * @return current index
@@ -170,7 +193,7 @@ public class CodeEntityList { // anpassen an CELIST
      * @param index current index to set
      */
     public void setIndex(int index) {
-        if (index < this.size() && index > -1) {
+        if (index < this.size() + 1 && index > -1) {
             this.index = index;
         }
     }
@@ -226,6 +249,20 @@ public class CodeEntityList { // anpassen an CELIST
         return bool;
     }
 
+    /**
+     *
+     * @return list with code entity names
+     */
+    public ArrayList<String> getNames() {
+        ArrayList<String> names = new ArrayList<>();
+
+        for (CodeEntity codeEntity : getEntities()) {
+            names.add(getEntityName(codeEntity));
+        }
+
+        return names;
+    }
+
     @Override
     public int hashCode() {
         int hash = 5;
@@ -234,15 +271,32 @@ public class CodeEntityList { // anpassen an CELIST
         return hash;
     }
 
+    /**
+     *
+     * @param entity code entity
+     * @return name as string
+     */
     private String getEntityName(CodeEntity entity) {
         return SimilarityMetric.getCodeEntityName(entity);
     }
 
+    /**
+     *
+     * @param i position in the list with code entities
+     * @return name of code entity with pos i as string
+     */
     public String getEntityName(int i) {
         return getEntityName(this.entities.get(i));
     }
 
-    private boolean compareNames(CodeEntity codeEntity1, CodeEntity codeEntity2) {
+    /**
+     *
+     * @param codeEntity1 code entity
+     * @param codeEntity2 code entity
+     * @return true if entity names are equal and the code entities are from the
+     * same type
+     */
+    public boolean compareNames(CodeEntity codeEntity1, CodeEntity codeEntity2) {
 
         if (codeEntity1 == null || codeEntity2 == null || codeEntity1.getClass() != codeEntity2.getClass()) {
             return false;
@@ -250,11 +304,22 @@ public class CodeEntityList { // anpassen an CELIST
         return getEntityName(codeEntity1).equals(getEntityName(codeEntity2));
     }
 
+    /**
+     *
+     * @param codeEntity1 code entity
+     * @param codeEntity2 code entity
+     * @return true if entity names are equal
+     */
     public boolean compNames(CodeEntity codeEntity1, CodeEntity codeEntity2) {
         return getEntityName(codeEntity1).equals(getEntityName(codeEntity2));
     }
 
-    private ArrayList<CodeEntity> convertTreeToList(Scope scope) { 
+    /**
+     *
+     * @param scope root element
+     * @return list of code entities - scopes and variables
+     */
+    private ArrayList<CodeEntity> convertTreeToList(Scope scope) {
         ArrayList<CodeEntity> codeEntities = new ArrayList();
         scope.visitScopeAndAllSubElements((CodeEntity e) -> {
             if (e instanceof Scope || e instanceof Variable) {
@@ -265,31 +330,81 @@ public class CodeEntityList { // anpassen an CELIST
         return codeEntities;
     }
 
-    public void updateList(CodeEntity codeEntity) {
-            ArrayList<CodeEntity> codeEntities = new ArrayList();
-            Scope root = (Scope) codeEntity;
+    public static int subtreeSize(Scope root) {
+        ArrayList<CodeEntity> codeEntities = new ArrayList();
+        root.visitScopeAndAllSubElements((CodeEntity e) -> {
+            if (e instanceof Scope || e instanceof Variable) {
+                codeEntities.add(e);
+            }
+        });
+        return codeEntities.size() - 1;
 
-            root.visitScopeAndAllSubElements((CodeEntity e) -> {
-                if (e instanceof Scope || e instanceof Variable) {
-                    codeEntities.add(e);
-                }
-            });
-            
-           for (int i = 0; i < codeEntities.size(); i++) {
-               System.out.println("Update: " + SimilarityMetric.getCodeEntityName(codeEntities.get(i)) );
-            
-        }
-            setEntities(codeEntities);
     }
-    
-    public CodeEntity getRoot(CodeEntity codeEntity) {
+
+    /**
+     *
+     * @param codeEntity code entity
+     */
+    public void updateCodeEntityList(CodeEntity codeEntity) {
+        ArrayList<CodeEntity> codeEntities = new ArrayList();
+        CompilationUnitDeclaration cudClone = (CompilationUnitDeclaration) getRoot(codeEntity);
+        try {
+            cudClone = clone(cudClone);
+        } catch (Exception ex) {
+            Logger.getLogger(CodeEntityList.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        cudClone.visitScopeAndAllSubElements((CodeEntity e) -> {
+            if (e instanceof Scope || e instanceof Variable) {
+                codeEntities.add(e);
+            }
+        });
+
+        for (int i = 0; i < codeEntities.size(); i++) {
+            System.out.println("Update: " + SimilarityMetric.getCodeEntityName(codeEntities.get(i)));
+
+        }
+        setEntities(codeEntities);
+    }
+
+    /**
+     *
+     */
+    public void updateCodeEntityList() {
+        ArrayList<CodeEntity> codeEntities = new ArrayList();
+        CompilationUnitDeclaration cudClone = cud;
+        try {
+            cudClone = clone(cud);
+        } catch (Exception ex) {
+            Logger.getLogger(CodeEntityList.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        cudClone.visitScopeAndAllSubElements((CodeEntity e) -> {
+            if (e instanceof Scope || e instanceof Variable) {
+                codeEntities.add(e);
+            }
+        });
+
+        for (int i = 0; i < codeEntities.size(); i++) {
+            System.out.println("Update_new: " + SimilarityMetric.getCodeEntityName(codeEntities.get(i)));
+
+        }
+        setEntities(codeEntities);
+    }
+
+    /**
+     *
+     * @param codeEntity child node
+     * @return root of the tree
+     */
+    public static CodeEntity getRoot(CodeEntity codeEntity) {
         CodeEntity ce = codeEntity;
         while (ce.getParent() != null) {
             ce = ce.getParent();
         }
         return ce;
     }
-    
+
     public CompilationUnitDeclaration groovy2Model(String groovyCode) throws Exception {
         SourceUnit src = fromCode(groovyCode);
         CompositeTransformingVisitorSupport visitor = VRLVisualizationTransformation
@@ -305,7 +420,7 @@ public class CodeEntityList { // anpassen an CELIST
         return Scope2Code.getCode(cuDecl);
     }
 
-    public CompilationUnitDeclaration clone(CompilationUnitDeclaration input) throws Exception {
+    private CompilationUnitDeclaration clone(CompilationUnitDeclaration input) throws Exception {
         return groovy2Model(model2Groovy(input));
     }
 }
