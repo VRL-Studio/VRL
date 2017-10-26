67b99961ccd52b06f196f48400fb89a5ebb3b18a
Michael Hoffer <info@michaelhoffer.de>
2017-10-26 18:21:24 +0200

    preparing new version

--- VRL/src/eu/mihosoft/vrl/system/Constants.java
+++ VRL/src/eu/mihosoft/vrl/system/Constants.java
@@ -75,7 +75,7 @@ public class Constants {
     /**
      * VRL version number that indicates major and minor changes.
      */
-    public static final String VERSION_BASE = VERSION_MAJOR + ".2.1";
+    public static final String VERSION_BASE = VERSION_MAJOR + ".2.2";
     // "-HEAD" or "-unstable" or "" for release etc.
     public static final String VERSION_SUFFIX = "";
     // full version (base+suffix+date)
