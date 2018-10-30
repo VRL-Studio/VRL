ab4725f96c62496a40189275975cf2035f367546
Michael Hoffer <info@michaelhoffer.de>
2018-10-30 22:56:11 +0100

    ready for the next minor release

--- VRL/src/eu/mihosoft/vrl/system/Constants.java
+++ VRL/src/eu/mihosoft/vrl/system/Constants.java
@@ -76,7 +76,7 @@ public class Constants {
     /**
      * VRL version number that indicates major and minor changes.
      */
-    public static final String VERSION_BASE = VERSION_MAJOR + ".0.1";
+    public static final String VERSION_BASE = VERSION_MAJOR + ".0.2";
     // "-HEAD" or "-unstable" or "" for release etc.
     public static final String VERSION_SUFFIX = "";
     // full version (base+suffix+date)
