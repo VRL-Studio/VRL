5681eddd6b98b43aa98571e833a30791d2d5b7c8
Michael Hoffer <info@michaelhoffer.de>
2018-10-31 13:03:26 +0100

    Fixes issue #77 https://github.com/VRL-Studio/VRL/issues/77

--- VRL/src/eu/mihosoft/vrl/system/WithJarFileLocationInfo.java
+++ VRL/src/eu/mihosoft/vrl/system/WithJarFileLocationInfo.java
@@ -1,5 +1,5 @@
 /* 
- * PluginCacheEntry.java
+ * WithJarFileLocationInfo.java
  * 
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
@@ -53,100 +53,32 @@
  * Computing and Visualization in Science, 2013, 16(4),
  * 181–192. http://doi.org/10.1007/s00791-014-0230-y
  */
-
 package eu.mihosoft.vrl.system;
 
-import eu.mihosoft.vrl.io.IOUtil;
-import eu.mihosoft.vrl.io.VJarUtil;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author Michael Hoffer &lt;info@michaelhoffer.de&gt;
  */
-
-
-public class PluginCacheEntry {
-    private String jarFile;
-    private String configuratorClass;
-    private String jarFileChecksum;
-
-    public PluginCacheEntry() {
-
+public interface WithJarFileLocationInfo {
+    
+    /**
+     * Retuens the Jar file location of this plugin (only works if the plugin 
+     * has been loaded from the local file system).
+     * 
+     * @return jar file location
+     */
+    default File getJarFileLocation() {
+        return null;
     }
     
-    public PluginCacheEntry(PluginConfigurator pConf) {
-        configuratorClass = pConf.getClass().getName();
-        File f = VRL.getPluginJarFileLocation(pConf);
-        
-        jarFile = f.getName();
-        
-//        // make jarfile relative to plugin directory
-//        // plugin cache is 
-//        if (jarFile.startsWith(Constants.PLUGIN_DIR)) {
-//            jarFile = jarFile.substring(
-//                    Constants.PLUGIN_DIR.length()+1, jarFile.length());
-//        }
-        
-        if (!PluginCacheController.isChecksumsEnabled()) {
-            return;
-        }
-        
-        try {
-            jarFileChecksum = IOUtil.generateSHA1Sum(IOUtil.fileToByteArray(f));
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(PluginCacheEntry.class.getName()).
-                    log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(PluginCacheEntry.class.getName()).
-                    log(Level.SEVERE, null, ex);
-        }
-    }
-
     /**
-     * @return the jarFile
+     * Sets the jar file location of this plugin 
+     * (this method shouldn't be called by the plugin itself)
+     * @param f file to set
      */
-    public String getJarFile() {
-        return jarFile;
-    }
-
-    /**
-     * @param jarFile the jarFile to set
-     */
-    public void setJarFile(String jarFile) {
-        this.jarFile = jarFile;
-    }
-
-    /**
-     * @return the configuratorClass
-     */
-    public String getConfiguratorClass() {
-        return configuratorClass;
-    }
-
-    /**
-     * @param configuratorClass the configuratorClass to set
-     */
-    public void setConfiguratorClass(String configuratorClass) {
-        this.configuratorClass = configuratorClass;
-    }
-
-    /**
-     * @return the jarFileChecksum
-     */
-    public String getJarFileChecksum() {
-        return jarFileChecksum;
-    }
-
-    /**
-     * @param jarFileChecksum the jarFileChecksum to set
-     */
-    public void setJarFileChecksum(String jarFileChecksum) {
-        this.jarFileChecksum = jarFileChecksum;
+    default void setJarFileLocation(File f) {
+        throw new UnsupportedOperationException("setPluginJarLocation is not supported.");
     }
 }
